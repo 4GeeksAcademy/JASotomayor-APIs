d8ffd07bfb4df3acaa0b4d4ee93f574b6db8f228
JASotomayor <jose.antonio.sotomayor.martinez@gmail.com>
2025-05-14 18:04:27 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -8,3 +8,20 @@ load_dotenv()
 
 
 # Spotify API credentials
+import os
+import pandas as pd
+import seaborn as sns
+from dotenv import load_dotenv
+
+# load the .env file variables
+load_dotenv()
+
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
